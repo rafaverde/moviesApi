8b96858271ca318fb798ad4c4d009d731ce10567
rafaverde <rafaverde@msn.com>
2024-05-04 09:13:17 -0300

    Cors configured

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors")
 
 const migrationsRun = require("./database/sqlite/migrations")
+const cors = require("cors")
 
 const AppError = require("./utils/AppError")
 
@@ -11,6 +12,8 @@ const routes = require("./routes")
 migrationsRun()
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use(routes)
